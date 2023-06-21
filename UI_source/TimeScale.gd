c0fe9915fa3f79fe50414b5843f138be0893ea08
3Disney <victorcauzz@hotmail.com>
2023-06-21 15:29:07 -0300

    TimeScale manipulation Fixed
    Só falta ajeitar 2 coisas:
    - Fazer com que o zoom e o giro de camera não dependam do time scale (opcional)
    - Fazer que a interface não possa ser controlada pelo teclado

---
+++ UI_source/TimeScale.gd
@@ -0,0 +1,14 @@
+extends HSlider
+@onready var texto : Label = get_children()[0]
+@onready var timeScale : int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (value != 0) :
+		Engine.time_scale = value
+	texto.text = "Time Scale: " + str(value)
